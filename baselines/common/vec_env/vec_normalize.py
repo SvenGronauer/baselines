FIX: prevent reward scaling during evaluation

baselines/common/vec_env/vec_normalize.py
@@ -32,8 +32,8 @@ class VecNormalize(VecEnvWrapper):
         obs, rews, news, infos = self.venv.step_wait()
         self.ret = self.ret * self.gamma + rews
         obs = self._obfilt(obs)
-        if self.ret_rms:
-            self.ret_rms.update(self.ret) if self.training else None
+        if self.ret_rms and self.training:
+            self.ret_rms.update(self.ret)
             rews = np.clip(rews / np.sqrt(self.ret_rms.var + self.epsilon), -self.cliprew, self.cliprew)
         self.ret[news] = 0.
         return obs, rews, news, infos